c74f557b07a126789831eb1b7b40169720c1f5f3
George Pastushok <gpastushok@yahoo.com>
2022-03-15 00:45:21 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// print the splash image to the terminal
+
+// run the inquirer prompts
+    // list database tasks
+    // what would you like to do?
+        // view all employees
+            // get employees from mysql instance
+        // add employee
+            // employee name?
+            // employee role?
+            // employee's manager?
+        // update
+            // select employee
+            // select role
+        // view roles
+            // get all roles from db
+        // add role
+            // prompt for role name
+            // for the salary
+            // for the department the role belongs to
+        // view depts
+            // get a list of all depts from the db
+        // add depts
+            // add dept to the db
+        // quit
